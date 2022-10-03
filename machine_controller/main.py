654ab4296f060cead6e2e6d52edd2663354a8cf5
mnemonia <birkeland@gmx.net>
2022-10-03 12:52:39 +0200

    Add machine controller

--- machine_controller/main.py
+++ machine_controller/main.py
@@ -1,1 +1,11 @@
+import asyncio
+
+import aioserial
+
 print("Kessler Machine Controller")
+
+async def read_and_print(aioserial_instance: aioserial.AioSerial):
+    while True:
+        print((await aioserial_instance.read_async()).decode(errors='ignore'), end='', flush=True)
+
+asyncio.run(read_and_print(aioserial.AioSerial(port='COM7')))
